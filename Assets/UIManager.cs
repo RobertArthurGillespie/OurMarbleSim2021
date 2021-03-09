83deb2ee8f138b1afa5f1208e7aa550fc3b828a0
RobertArthurGillespie <RobertArthurGillespie@outlook.com>
2021-03-09 16:58:24 -0600

    commit 3/9
    Commit March 9

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -2,14 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class UIManager : MonoBehaviour,IPointerClickHandler
+public class UIManager : MonoBehaviour
 {
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        Debug.Log("you clicked on me");
-        
-    }
+    string currentSelectedUIElementName;
+
+    [SerializeField]
+    Dropdown dropDown;
+    [SerializeField]
+    Image iconImage;
+
+    //graphics raycasting variables
+    [SerializeField]
+    GraphicRaycaster graphicRaycaster;
+    [SerializeField]
+    EventSystem eventSystem;
+    PointerEventData eventData;
+    List<RaycastResult> gRResults = new List<RaycastResult>();
+
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,6 +32,35 @@ public class UIManager : MonoBehaviour,IPointerClickHandler
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetMouseButtonDown(0))
+        {
+            eventData = new PointerEventData(eventSystem);
+            eventData.position = Input.mousePosition;
+            graphicRaycaster.Raycast(eventData, gRResults);
+            if (gRResults.Count > 0)
+            {
+                currentSelectedUIElementName = gRResults[gRResults.Count - 1].gameObject.name;
+                Debug.Log("latest raycast result is: " + currentSelectedUIElementName);
+
+                foreach (RaycastResult rr in gRResults)
+                {
+
+                    Debug.Log("raycast results are: " + rr.gameObject.name);
+                }
+            }
+        }
         
+
+
+    }
+
+    public void WriteToConsole()
+    {
+        Debug.Log("Yay you wrote to console");
+    }
+
+    public void ChangeIcon()
+    {
+        iconImage.sprite = dropDown.options[dropDown.value].image;
     }
 }
---
+++ Assets/UISelectEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class UISelectEvent : MonoBehaviour,IPointerClickHandler
+{
+    [SerializeField]
+    UnityEvent OnSelectUI;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+       
+
+        if (eventData.pointerPress.gameObject.name == "Image")
+        {
+            Debug.Log("You are clicking on the Image!");
+        }
+        OnSelectUI.Invoke();
+    }
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
